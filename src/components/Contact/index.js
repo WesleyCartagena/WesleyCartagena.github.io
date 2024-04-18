c7745ad431d9cc4cc4a63fad4c9810c257e997d5
wcartagenaasc <wesleycartagena23@gmail.com>
2024-04-18 19:54:07 -0400

    Removed Contact Page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,71 +1,71 @@
-import './index.scss'
-import { useEffect, useState, useRef } from 'react'
-import emailjs from '@emailjs/browser';
-import AnimatedLetters from '../AnimatedLetters/Index'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareEnvelope, faMapLocation} from '@fortawesome/free-solid-svg-icons'
+// import './index.scss'
+// import { useEffect, useState, useRef } from 'react'
+// import emailjs from '@emailjs/browser';
+// import AnimatedLetters from '../AnimatedLetters/Index'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faSquareEnvelope, faMapLocation} from '@fortawesome/free-solid-svg-icons'
 
 
-const Contact = () => {
-  const[letterClass, setLetterClass]= useState('text-animate')
-  const contactArray = ['Contact',' ','Me']
-  useEffect(() => {
-    setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-    return () => clearTimeout()
-    }, []);
-   const form = useRef()
-   const sendEmail = (e) => {
-    e.preventDefault();
+// const Contact = () => {
+//   const[letterClass, setLetterClass]= useState('text-animate')
+//   const contactArray = ['Contact',' ','Me']
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setLetterClass('text-animate-hover')
+//     }, 4000)
+//     return () => clearTimeout()
+//     }, []);
+//    const form = useRef()
+//    const sendEmail = (e) => {
+//     e.preventDefault();
 
-    emailjs.sendForm('service_bd548m8', 'template_nntslif', form.current, 'UKu0PNHK_5BBZMjRr')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset()
-    };
-    return(
-        <section className ='container contact-page'>
-        <div className="form-container">
-          <form ref={form} onSubmit={sendEmail} className="contact-form">
-            <div className='contact-text-zone'>
-              <span>
-                <AnimatedLetters letterClass={letterClass}
-                  strArray={contactArray} 
-                  idx={15}
-               />
-              </span>             
-            </div> 
-            <input type="text"  className="feedback-input" placeholder='Full Name' name='user_name' required/>
-            <input type="email"  className="feedback-input" placeholder='email' name='user_email' required/>
-            <input type="text" className="feedback-input" placeholder='subject' name='subject' required/>
-            <textarea className="feedback-input" name="message" cols="30" rows="10"></textarea>
-            <button type="submit"className="btn">Send Message</button>
-            <div className="contact-icons">
-            <ul id="horizontal-list-contact">
-              <li>
-                <span className="icons">
+//     emailjs.sendForm('service_bd548m8', 'template_nntslif', form.current, 'UKu0PNHK_5BBZMjRr')
+//       .then((result) => {
+//           console.log(result.text);
+//       }, (error) => {
+//           console.log(error.text);
+//       });
+//       e.target.reset()
+//     };
+//     return(
+//         <section className ='container contact-page'>
+//         <div className="form-container">
+//           <form ref={form} onSubmit={sendEmail} className="contact-form">
+//             <div className='contact-text-zone'>
+//               <span>
+//                 <AnimatedLetters letterClass={letterClass}
+//                   strArray={contactArray} 
+//                   idx={15}
+//                />
+//               </span>             
+//             </div> 
+//             <input type="text"  className="feedback-input" placeholder='Full Name' name='user_name' required/>
+//             <input type="email"  className="feedback-input" placeholder='email' name='user_email' required/>
+//             <input type="text" className="feedback-input" placeholder='subject' name='subject' required/>
+//             <textarea className="feedback-input" name="message" cols="30" rows="10"></textarea>
+//             <button type="submit"className="btn">Send Message</button>
+//             <div className="contact-icons">
+//             <ul id="horizontal-list-contact">
+//               <li>
+//                 <span className="icons">
                   
-                <FontAwesomeIcon icon={faSquareEnvelope} color="#32CD30" />
-                </span>
-                <span className="info"> wesleycartagena23@gmail.com</span>
-              </li>
-              <li>
-                <span className="icons">
-                <FontAwesomeIcon icon={faMapLocation} color="#32CD30" />
-                </span>
-                <span className="info"> New York</span>
-              </li>
-            </ul>
-          </div>
-          </form>
-          </div>
-        </section>
-    );
-}
+//                 <FontAwesomeIcon icon={faSquareEnvelope} color="#32CD30" />
+//                 </span>
+//                 <span className="info"> </span>
+//               </li>
+//               <li>
+//                 <span className="icons">
+//                 <FontAwesomeIcon icon={faMapLocation} color="#32CD30" />
+//                 </span>
+//                 <span className="info"> New York</span>
+//               </li>
+//             </ul>
+//           </div>
+//           </form>
+//           </div>
+//         </section>
+//     );
+// }
 
-export default Contact
+// export default Contact
 
