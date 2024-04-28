1d2617302dd53e183961f945154ff687a6eafeb2
Wesley Cartagena <wesleycartagena23@gmail.com>
2024-04-28 15:53:57 -0400

    Rebuilt Navbar in bootstrap and made it more user friendly

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import { Route, Routes} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
-import AboutTest from './components/AboutTest'
 //import Contact from './components/Contact'
 import Layout from './components/Layout'
 import Projects from './components/Projects'
 import './App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About/>}/>
-          <Route path="aboutTest" element={<AboutTest/>}/>
+           {/*<Route index element={<Home />} />*/}
+           {/*<Route path="about" element={<About/>}/>*/}
           {/*<Route path="/contact" element={<Contact/>}/>*/}
-          <Route path="/Projects" element={<Projects/>}/>
+           {/*<Route path="/Projects" element={<Projects/>}/>*/}
         </Route>
       </Routes>
     </>
